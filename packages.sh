a7d51436407a8417d82b1e7268ba4a911d4c950a
p0wer0n <jaredebenstein@gmail.com>
2015-10-17 12:57:34 -0400

    Added AppSync

---
+++ packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dpkg-deb -b projects/appsyncunified debs
+# dpkg-deb -bZgzip projects/<project name> <output folder>
---
+++ remove.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find debs -type f -name '*.deb' -delete
+rm -rf Packages.bz2
